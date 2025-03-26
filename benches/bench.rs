e4694048a95c58b6038b789820981c53538e2602
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2025-03-26 11:49:28 -0400

    Disable sparse bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,16 +9,16 @@ fn bench(c: &mut Criterion) {
     let mut group = c.benchmark_group("storage-solution");
 
     let voxel_generator = VoxelGenerator::new();
-    let bb = IAabb::new(IVec3::ZERO, 1000 * IVec3::ONE);
+    let bb = IAabb::new(IVec3::ZERO, 100 * IVec3::ONE);
 
     let dense_ray_tracer = RayTracer::<DenseStorage>::from_voxels(&voxel_generator, bb);
-    let sparse_ray_tracer = RayTracer::<SparseStorage>::from_voxels(&voxel_generator, bb);
+    // let sparse_ray_tracer = RayTracer::<SparseStorage>::from_voxels(&voxel_generator, bb);
 
     group.bench_function("dense", |b| b.iter(|| black_box(dense_ray_tracer.render())));
 
-    group.bench_function("sparse", |b| {
-        b.iter(|| black_box(sparse_ray_tracer.render()))
-    });
+    // group.bench_function("sparse", |b| {
+    //     b.iter(|| black_box(sparse_ray_tracer.render()))
+    // });
 }
 
 criterion_group!(benches, bench);
